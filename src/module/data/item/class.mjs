58eca650faeefd96c038a57b27f168d429b6cd4f
Joseph <JPMeehan5@Gmail.com>
2024-07-28 21:52:32 -0700

    First draft of ClassModel.defineSchema

--- src/module/data/item/class.mjs
+++ src/module/data/item/class.mjs
@@ -14,6 +14,24 @@ export default class ClassModel extends BaseItemModel {
     const fields = foundry.data.fields;
     const schema = super.defineSchema();
 
+    schema.characteristics = new fields.SchemaField({
+      core: new fields.SetField(new fields.StringField({choices: CONFIG.DRAW_STEEL.characteristics, required: true}))
+    });
+
+    schema.stamina = new fields.SchemaField({
+      starting: new fields.NumberField({required: true, initial: 20}),
+      level: new fields.NumberField({required: true, initial: 12})
+    });
+
+    schema.skills = new fields.SchemaField({
+      options: new fields.SetField(new fields.StringField({choices: this.skillOptions})),
+      count: new fields.NumberField(),
+      choices: new fields.SetField(new fields.StringField({blank: true, required: true, choices: this.skillChoice}))
+    });
+
+    // TODO: Copy 5e? Huge risk of changes here
+    schema.advancement = new fields.ObjectField();
+
     return schema;
   }
 }
