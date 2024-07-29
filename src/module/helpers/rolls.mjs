9fc19c28e00270d343c761f60369acd9b839ed15
Joseph <JPMeehan5@Gmail.com>
2024-07-29 13:08:13 -0700

    More PowerRoll adjustments

--- src/module/helpers/rolls.mjs
+++ src/module/helpers/rolls.mjs
@@ -8,8 +8,31 @@ export class DSRoll extends Roll {}
  */
 export class PowerRoll extends DSRoll {
   constructor(formula = "2d10", data = {}, options = {}) {
+    // TODO: Figure out how hard to code the formula
     super(formula, data, options);
+    if (this.options.type === undefined) this.options.type = "test";
+    else if (!PowerRoll.VALID_TYPES.has(this.options.type)) throw new Error("Power rolls must be an ability, resistance, or test");
     if (this.options.criticalThreshold === undefined) this.options.criticalThreshold = 19;
+  }
+
+  /**
+   * Types of Power Rolls
+   */
+  static get TYPES() {
+    return PowerRoll.#TYPES;
+  }
+
+  static #TYPES = Object.freeze({
+    ABILITY: "ability",
+    RESISTANCE: "resistance",
+    TEST: "test"
+  });
+
+  /**
+   * Set of power roll types
+   */
+  static get VALID_TYPES() {
+    return new Set(Object.values(this.#TYPES));
   }
 
   /**
@@ -18,7 +41,7 @@ export class PowerRoll extends DSRoll {
    */
   get product() {
     if (this._total === undefined) return undefined;
-    else if (this._total < 11) return 1;
+    else if (this._total < 11) return 1; // TODO: Possibly adjust these thresholds to be configurable
     else if (this._total < 17) return 2;
     else return 3;
   }
@@ -31,10 +54,28 @@ export class PowerRoll extends DSRoll {
   }
 
   /**
-   * Determines if this power roll was a critical
-   * @returns {boolean | null} Null if not yet evaluated, otherwise returns true if the dice total is a 19 or higher
+   * Returns the natural result of the power roll
+   */
+  get naturalResult() {
+    return this.dice[0].total;
+  }
+
+  /**
+   * Determines if the natural result was a natural 20
+   * @returns {boolean | null} Null if not yet evaluated
+   */
+  get nat20() {
+    if (this._total === undefined) return null;
+    return (this.dice[0].total >= 20);
+  }
+
+  /**
+   * Determines if an ability power roll was a critical
+   * @returns {boolean | null} Null if not yet evaluated or not an ability roll,
+   * otherwise returns true if the dice total is a 19 or higher
    */
   get critical() {
+    if (this.options.type !== "ability") return null;
     if (this._total === undefined) return null;
     return (this.dice[0].total >= this.options.criticalThreshold);
   }
