621329467b2373ba34849913f9eb374537c59e7d
Joseph <JPMeehan5@Gmail.com>
2024-09-01 21:47:35 -0700

    More header work

--- src/module/apps/_module.mjs
+++ src/module/apps/_module.mjs
@@ -1,2 +1,8 @@
 export {DrawSteelActorSheet} from "./actor-sheet.mjs";
 export {DrawSteelItemSheet} from "./item-sheet.mjs";
+
+import SlideToggleElement from "./slide-toggle.mjs";
+
+window.customElements.define("slide-toggle", SlideToggleElement);
+
+export {SlideToggleElement};
