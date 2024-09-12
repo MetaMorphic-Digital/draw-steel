1d1c6479bf20e676a9af48953efd7b722f8ac1b4
Taroco <taroco@eclipso.de>
2024-09-12 19:28:57 +0200

    fix create-symlink error by importing and using promise based fs

--- tools/create-symlinks.mjs
+++ tools/create-symlinks.mjs
@@ -1,18 +1,24 @@
 import * as fs from "fs";
+import {promises as fsPromises} from "fs";
 import yaml from "js-yaml";
 import path from "path";
 
 console.log("Reforging Symlinks");
 
 if (fs.existsSync("foundry-config.yaml")) {
-  const fc = await fs.readFile("foundry-config.yaml");
+  let fileRoot = "";
+  try {
+    const fc = await fsPromises.readFile("foundry-config.yaml", "utf-8");
 
-  const foundryConfig = yaml.load(fc);
-
-  const fileRoot = path.join(foundryConfig.installPath, "resources", "app");
+    const foundryConfig = yaml.load(fc);
+  
+    fileRoot = path.join(foundryConfig.installPath, "resources", "app");
+  } catch (err) {
+    console.error(`Error reading foundry-config.yaml: ${err}`);
+  }
 
   try {
-    await fs.mkdir("foundry");
+    await fsPromises.mkdir("foundry");
   } catch (e) {
     if (e.code !== "EEXIST") throw e;
   }
@@ -20,7 +26,7 @@ if (fs.existsSync("foundry-config.yaml")) {
   // Javascript files
   for (const p of ["client", "client-esm", "common"]) {
     try {
-      await fs.symlink(path.join(fileRoot, p), path.join("foundry", p));
+      await fsPromises.symlink(path.join(fileRoot, p), path.join("foundry", p));
     } catch (e) {
       if (e.code !== "EEXIST") throw e;
     }
@@ -28,7 +34,7 @@ if (fs.existsSync("foundry-config.yaml")) {
 
   // Language files
   try {
-    await fs.symlink(path.join(fileRoot, "public", "lang"), path.join("foundry", "lang"));
+    await fsPromises.symlink(path.join(fileRoot, "public", "lang"), path.join("foundry", "lang"));
   } catch (e) {
     if (e.code !== "EEXIST") throw e;
   }
