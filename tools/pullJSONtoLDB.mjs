51cfaf45cf1ff7f73f9d9716f1e923aa12d8c9de
Taroc0 <111232848+Taroc0@users.noreply.github.com>
2024-10-10 07:06:46 +0200

    Pack and unpack with compendium folders that match real folders in so… (#44)
    * Pack and unpack with compendium folders that match real folders in source directory.
    
    * Fixed naming

--- tools/pullJSONtoLDB.mjs
+++ tools/pullJSONtoLDB.mjs
@@ -1,14 +1,28 @@
 import {compilePack} from "@foundryvtt/foundryvtt-cli";
 import {promises as fs} from "fs";
+import path from "path";
 
-const MODULE_ID = process.cwd();
+const SYSTEM_ID = process.cwd();
 
-const packs = await fs.readdir("./src/packs");
-for (const pack of packs) {
-  if (pack === ".gitattributes") continue;
-  console.log("Packing " + pack);
-  await compilePack(
-    `${MODULE_ID}/src/packs/${pack}`,
-    `${MODULE_ID}/packs/${pack}`
-  );
+const BASE_SRC_PATH = "src/packs";
+const BASE_DEST_PATH = "packs";
+
+await compilePacksRecursivly();
+
+/**
+ * Compiles all packs in the given base path
+ */
+async function compilePacksRecursivly() {
+  const packs = (await fs.readdir(BASE_SRC_PATH, {withFileTypes: true})).filter(file => file.isDirectory());
+  
+  for (const pack of packs) {
+    const srcPath = path.join(BASE_SRC_PATH, pack.name);
+    const destPath = path.join(BASE_DEST_PATH, pack.name);
+    console.log("Packing " + srcPath + " to " + destPath);
+    await compilePack(
+      `${SYSTEM_ID}/${srcPath}`,
+      `${SYSTEM_ID}/${destPath}`, 
+      {recursive: true, log: true}
+    );
+  }
 }
--- tools/pushLDBtoJSON.mjs
+++ tools/pushLDBtoJSON.mjs
@@ -2,29 +2,86 @@ import {extractPack} from "@foundryvtt/foundryvtt-cli";
 import {promises as fs} from "fs";
 import path from "path";
 
-const MODULE_ID = process.cwd();
+const SYSTEM_ID = process.cwd();
+const BASE_LDB_PATH = "packs";
+const BASE_SRC_PATH = "src";
 
-const packs = await fs.readdir("./packs");
-for (const pack of packs) {
-  if (pack === ".gitattributes") continue;
-  console.log("Unpacking " + pack);
-  const directory = `./src/packs/${pack}`;
-  try {
-    for (const file of await fs.readdir(directory)) {
-      await fs.unlink(path.join(directory, file));
-    }
-  } catch (error) {
-    if (error.code === "ENOENT") console.log("No files inside of " + pack);
-    else console.log(error);
+await extractPacks();
+
+/**
+ * Unpacks all compendium packs located in the basePath
+ */
+async function extractPacks() {
+  const dirents = await fs.readdir(BASE_LDB_PATH, {withFileTypes: true, recursive: true});
+  const packs = dirents.filter((dirent) => dirent.isDirectory());
+
+  const folders = {};
+  for (const pack of packs) {
+    const packName = path.join(pack.path, pack.name);
+    console.log("Unpacking " + packName);
+    await extractAllFoldersFromPackFile(folders, packName);
+    Object.values(folders).forEach(folder => buildAndAddPath(folders, folder));
+    await unpackToPath(folders, packName);
   }
+}
+
+/**
+ * Reads all folders inside a pack and adds it to a collection of folders
+ * @param {ref} collection - Reference to the collection of folders
+ * @param {string} packName - Path to the pack ldb folder
+ */
+async function extractAllFoldersFromPackFile(collection, packName) {
   await extractPack(
-    `${MODULE_ID}/packs/${pack}`,
-    `${MODULE_ID}/src/packs/${pack}`,
+    `${SYSTEM_ID}/${packName}`,
+    `${SYSTEM_ID}/${BASE_SRC_PATH}/${packName}`,
     {
-      transformName
+      transformEntry: (entry) => {
+        if (entry._key.startsWith("!folders")) {
+          collection[entry._id] = {name: slugify(entry.name), parentFolder: entry.folder};
+        }
+        return false;
+      }
     }
   );
 }
+
+/**
+ * Adds a path to each folder in the collection
+ * @param {ref} collection - Reference to the collection of folders
+ * @param {object} folderEntry - An entry from the folders collection
+ */
+function buildAndAddPath(collection, folderEntry) {
+  let parent = collection[folderEntry.parentFolder];
+  folderEntry.path = folderEntry.name;
+  while (parent) {
+    folderEntry.path = path.join(parent.path, folderEntry.path);
+    parent = collection[parent.parentFolder];
+  }
+}
+
+/**
+ * Unpacks all files and folder from the given packName path
+ * Files are unpacked into their respective compendium folders
+ * @param {ref} collection - Reference to the collection of folders
+ * @param {string} packName - Path to the pack ldb folder
+ */
+async function unpackToPath(collection, packName) {
+  await extractPack(
+    `${SYSTEM_ID}/${packName}`,
+    `${SYSTEM_ID}/${BASE_SRC_PATH}/${packName}`,
+    {
+      transformName: entry => {
+        const filename = transformName(entry);
+        if (entry._id in collection) {
+          return path.join(collection[entry._id].path, filename);
+        }
+        const parent = collection[entry.folder];
+        return path.join(parent?.path ?? "", filename);
+      }
+    }
+  );
+}
+
 /**
  * Prefaces the document with its type
  * @param {object} doc - The document data
@@ -35,4 +92,13 @@ function transformName(doc) {
   const prefix = ["actors", "items"].includes(type) ? doc.type : type;
 
   return `${doc.name ? `${prefix}_${safeFileName}_${doc._id}` : doc._id}.json`;
+}
+
+/**
+ * Standardize name format.
+ * @param {string} name
+ * @returns {string}
+ */
+function slugify(name) {
+  return name.toLowerCase().replace("'", "").replace(/[^a-z0-9]+/gi, " ").trim().replace(/\s+|-{2,}/g, "-");
 }
