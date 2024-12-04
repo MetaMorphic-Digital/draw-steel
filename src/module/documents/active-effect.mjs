e32eee1dc3552cfe819489eb564d5cb9a33b9b5e
Joseph <JPMeehan5@Gmail.com>
2024-12-03 17:48:50 -0800

    Suppress effects with expired durations

--- src/module/documents/active-effect.mjs
+++ src/module/documents/active-effect.mjs
@@ -1,4 +1,15 @@
 export class DrawSteelActiveEffect extends ActiveEffect {
+
+  /**
+   * Automatically deactivate effects with expired durations
+   */
+  get isSuppressed() {
+    if (Number.isNumeric(this.duration?.remaining)) {
+      return this.duration?.remaining <= 0;
+    }
+    return false;
+  }
+
   /** @override */
   prepareDerivedData() {
     super.prepareDerivedData();
