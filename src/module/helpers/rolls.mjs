66ae34002b21ebd403544c754a6ef8e579a38207
JPMeehan <JPMeehan5@gmail.com>
2024-12-21 08:56:23 -0800

    Implemented Saving Throw

--- src/module/helpers/rolls.mjs
+++ src/module/helpers/rolls.mjs
@@ -509,3 +509,43 @@ export class ProjectRoll extends DSRoll {
     return context;
   }
 }
+
+export class SavingThrow extends DSRoll {
+  /**
+   * @param {string} [formula="1d10"]      Most sav
+   * @param {Record<string, any>} data
+   * @param {Record<string, any>} options
+   */
+  constructor(formula = "1d10", data = {}, options = {}) {
+    super(formula, data, options);
+  }
+
+  static CHAT_TEMPLATE = systemPath("templates/rolls/save.hbs");
+
+  /**
+   * Did the saving throw succeed
+   * @returns {boolean}
+   */
+  get product() {
+    if (this._total === undefined) return undefined;
+    return this.total >= 6;
+  }
+
+  /**
+   * Helper function to generate render context in use with `static CHAT_TEMPLATE`
+   * @param {object} options
+   * @param {string} [options.flavor]     - Flavor text to include
+   * @param {boolean} [options.isPrivate] - Is the Roll displayed privately?
+   * @returns An object to be used in `renderTemplate`
+   */
+  async _prepareContext({flavor, isPrivate}) {
+    return {
+      formula: isPrivate ? "???" : this._formula,
+      flavor: isPrivate ? null : flavor ?? this.options.flavor ?? game.i18n.localize("DRAW_STEEL.Roll.Save.Label"),
+      user: game.user.id,
+      tooltip: isPrivate ? "" : await this.getTooltip(),
+      total: isPrivate ? "?" : Math.round(this.total * 100) / 100,
+      result: this.product ? "critical" : "failure"
+    };
+  }
+}
