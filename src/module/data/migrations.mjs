b787a1f1ff047902343035a4d106448261fcb2ee
Joseph <JPMeehan5@Gmail.com>
2025-01-11 08:49:17 -0800

    Added activeGM.isSelf check

--- src/module/data/migrations.mjs
+++ src/module/data/migrations.mjs
@@ -1,6 +1,15 @@
 import {systemID} from "../constants.mjs";
 
+/**
+ * Perform one-time migrations
+ * Run and awaited in the `ready` hook before `ds.ready` is called.
+ */
 export async function migrateWorld() {
+  // TODO: In v13 simplify to the new getter
+  if (!game.users.activeGM.isSelf) {
+    console.log("Not the active GM");
+    return;
+  }
   const migrationVersion = game.settings.get(systemID, "migrationVersion");
   if (!migrationVersion) {
     // New world - initialize the migration version and rename Gamemaster to Director
