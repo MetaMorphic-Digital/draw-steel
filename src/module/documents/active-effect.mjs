38af736d0170a0973493e1e21fd2129167e5c657
Kody Verrett <cdverrett94@gmail.com>
2025-01-16 18:10:29 -0600

    add targetedConditionPrompt

--- src/module/documents/active-effect.mjs
+++ src/module/documents/active-effect.mjs
@@ -1,9 +1,49 @@
+import {systemID} from "../constants.mjs";
+
 export class DrawSteelActiveEffect extends ActiveEffect {
   /** @override */
   static async _fromStatusEffect(statusId, effectData, options) {
     const effect = await super._fromStatusEffect(statusId, effectData, options);
-    if (effectData.rule) effect.updateSource({description: `@Embed[${effectData.rule} inline]`});
+    const sourceData = {};
+
+    if (effectData.rule) sourceData.description = `@Embed[${effectData.rule} inline]`;
+    if (["frightened", "grabbed", "taunted"].includes(statusId)) this.targetedConditionPrompt(statusId, sourceData);
+    
+    effect.updateSource(sourceData);
     return effect;
+  }
+
+  /**
+   * Modify the sourceData for the new effect with the changes to include the imposing actor's UUID in the appropriate flag.
+   * @param {string} statusId 
+   * @param {object} sourceData
+   */
+  static async targetedConditionPrompt(statusId, sourceData) {
+    try {
+      let imposingActorId = await foundry.applications.api.DialogV2.prompt({
+        content: `The imposing actor's UUID: 
+            <input type=text name="actorId" value="${game.user.targets.first()?.actor?.uuid ?? ""}" />
+          `,
+        ok: {
+          callback: (event, button, dialog) => dialog.querySelector("input").value
+        },
+        buttons: [{
+          action: "select-target",
+          label: "Select First Target's UUID",
+          callback: (event, button, dialog) => game.user.targets.first()?.actor?.uuid
+        }]
+      });
+  
+      if (foundry.utils.parseUuid(imposingActorId)) {
+        sourceData.changes = [{
+          key: `flags.${systemID}.${statusId}`,
+          mode: CONST.ACTIVE_EFFECT_MODES.OVERRIDE,
+          value: imposingActorId
+        }];
+      }
+    } catch (error) {
+      ui.notifications.error("An Invalid UUID was provided. Value was not set and automation won't apply");
+    }
   }
 
   /**
