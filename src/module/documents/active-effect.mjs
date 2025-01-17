c6b6be5b794126d25d1905dbe16faca6a145f19f
Kody Verrett <cdverrett94@gmail.com>
2025-01-17 13:59:20 -0600

    use consistent naming in targetedConditionPrompt

--- src/module/documents/active-effect.mjs
+++ src/module/documents/active-effect.mjs
@@ -12,17 +12,17 @@ export class DrawSteelActiveEffect extends ActiveEffect {
   }
 
   /**
-   * Modify the sourceData for the new effect with the changes to include the imposing actor's UUID in the appropriate flag.
+   * Modify the effectData for the new effect with the changes to include the imposing actor's UUID in the appropriate flag.
    * @param {string} statusId 
-   * @param {object} sourceData
+   * @param {object} effectData
    */
-  static async targetedConditionPrompt(statusId, sourceData) {
+  static async targetedConditionPrompt(statusId, effectData) {
     try {
       let imposingActorUuid = await TargetedConditionPrompt.prompt({context: {statusId}});
   
       if (foundry.utils.parseUuid(imposingActorUuid)) {
-        sourceData.changes = this.changes ?? [];
-        sourceData.changes.push({
+        effectData.changes = this.changes ?? [];
+        effectData.changes.push({
           key: `flags.${systemID}.${statusId}`,
           mode: CONST.ACTIVE_EFFECT_MODES.OVERRIDE,
           value: imposingActorUuid
