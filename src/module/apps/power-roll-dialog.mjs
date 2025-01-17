f69d15f8bb5fabced2166afed0cfd3e6ca0361e2
Kody Verrett <cdverrett94@gmail.com>
2025-01-17 13:06:59 -0600

    remove log

--- src/module/apps/power-roll-dialog.mjs
+++ src/module/apps/power-roll-dialog.mjs
@@ -106,7 +106,6 @@ export class PowerRollDialog extends HandlebarsApplicationMixin(ApplicationV2) {
     return new Promise((resolve, reject) => {
       const dialog = new this(options);
       dialog.addEventListener("close", event => {
-        console.log(dialog.promptValue);
         if (dialog.promptValue) resolve(dialog.promptValue);
         else resolve(null);
       }, {once: true});
