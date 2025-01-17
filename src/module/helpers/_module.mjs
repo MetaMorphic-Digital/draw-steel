c686d8ad683985f9e65571cb553203cea9d2cba5
Joseph <JPMeehan5@Gmail.com>
2025-01-17 15:14:34 -0800

    Implemented re-localizing data models on hotReload (#164)
    * Implemented re-localizing data models on hotReload
    
    * Added more comments
    
    * Update src/module/helpers/hotReload.mjs
    
    Co-authored-by: Zhell <50169243+krbz999@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Zhell <50169243+krbz999@users.noreply.github.com>

--- src/module/helpers/_module.mjs
+++ src/module/helpers/_module.mjs
@@ -1,4 +1,5 @@
 export * as macros from "./macros.mjs";
 export * as utils from "./utils.mjs";
+export * from "./hotReload.mjs";
 export {default as DrawSteelSettingsHandler} from "./settings.mjs";
 export {default as DrawSteelSocketHandler} from "./sockets.mjs";
