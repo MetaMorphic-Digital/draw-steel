c2960afde915991e0146a91a69ec74dee1de364e
Kody Verrett <cdverrett94@gmail.com>
2025-01-17 10:59:30 -0600

    Add prompt for targeted conditions (taunted, grabbed, frightened) (#170)
    * add targetedConditionPrompt
    
    * localization
    
    * making targeted conditions easily extensible
    
    * await
    
    * remove empty line
    
    * move to dedicated appv2 sheet
    
    * changes per review
    
    * AE sources changes
    
    * add icon

--- src/module/documents/active-effect.mjs
+++ src/module/documents/active-effect.mjs
@@ -1,9 +1,39 @@
+import {TargetedConditionPrompt} from "../apps/targeted-condition-prompt.mjs";
+import {systemID} from "../constants.mjs";
+
 export class DrawSteelActiveEffect extends ActiveEffect {
   /** @override */
   static async _fromStatusEffect(statusId, effectData, options) {
     const effect = await super._fromStatusEffect(statusId, effectData, options);
-    if (effectData.rule) effect.updateSource({description: `@Embed[${effectData.rule} inline]`});
+    const sourceData = {};
+
+    if (effectData.rule) sourceData.description = `@Embed[${effectData.rule} inline]`;
+    if (ds.CONFIG.conditions[statusId]?.targeted) await this.targetedConditionPrompt(statusId, sourceData);
+    
+    effect.updateSource(sourceData);
     return effect;
+  }
+
+  /**
+   * Modify the sourceData for the new effect with the changes to include the imposing actor's UUID in the appropriate flag.
+   * @param {string} statusId 
+   * @param {object} sourceData
+   */
+  static async targetedConditionPrompt(statusId, sourceData) {
+    try {
+      let imposingActorUuid = await TargetedConditionPrompt.prompt({context: {statusId}});
+  
+      if (foundry.utils.parseUuid(imposingActorUuid)) {
+        sourceData.changes = this.changes ?? [];
+        sourceData.changes.push({
+          key: `flags.${systemID}.${statusId}`,
+          mode: CONST.ACTIVE_EFFECT_MODES.OVERRIDE,
+          value: imposingActorUuid
+        });
+      }
+    } catch (error) {
+      ui.notifications.warn("DRAW_STEEL.Effect.TargetedConditionPrompt.Warning", {localize: true});
+    }
   }
 
   /**
