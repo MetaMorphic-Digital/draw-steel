76577d4322cb54ccffd3c506a9003440ac41f9e3
Kody Verrett <cdverrett94@gmail.com>
2025-01-16 18:39:30 -0600

    await

--- src/module/documents/active-effect.mjs
+++ src/module/documents/active-effect.mjs
@@ -7,7 +7,7 @@ export class DrawSteelActiveEffect extends ActiveEffect {
     const sourceData = {};
 
     if (effectData.rule) sourceData.description = `@Embed[${effectData.rule} inline]`;
-    if (ds.CONFIG.conditions[statusId]?.targeted) this.targetedConditionPrompt(statusId, sourceData);
+    if (ds.CONFIG.conditions[statusId]?.targeted) await this.targetedConditionPrompt(statusId, sourceData);
     
     effect.updateSource(sourceData);
     return effect;
