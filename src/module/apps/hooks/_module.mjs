4e73b04dfb95848767f27734dfac9d562760411b
Joseph <JPMeehan5@Gmail.com>
2025-01-22 18:02:25 -0800

    Improve display of Token Config

--- src/module/apps/hooks/_module.mjs
+++ src/module/apps/hooks/_module.mjs
@@ -2,3 +2,4 @@ export * from "./activeEffectConfig.mjs";
 export * from "./chatMessage.mjs";
 export * from "./combatantConfig.mjs";
 export * from "./combatTracker.mjs";
+export * from "./tokenConfig.mjs";
---
+++ src/module/apps/hooks/tokenConfig.mjs
@@ -0,0 +1,24 @@
+/** @import {DrawSteelActor} from "../../documents/actor.mjs" */
+
+/**
+ * A hook event that fires when the TokenConfig application is rendered
+ * @param {TokenConfig} app             The Application instance being rendered
+ * @param {JQuery<HTMLElement>} jquery  The inner HTML of the document that will be displayed and may be modified
+ * @param {Record<string, any>} context The object of data used when rendering the application
+ */
+export function renderTokenConfig(app, [html], context) {
+  /** @type {DrawSteelActor} */
+  const actor = app.document.actor;
+  // Replace option labels with schema-derived props
+  if (actor) {
+    const schema = actor.system.schema;
+    /** @type {HTMLSelectElement[]} */
+    const bars = html.querySelectorAll(".bar-attribute");
+    for (const bar of bars) {
+      for (const opt of bar.options) {
+        const field = schema.getField(opt.value);
+        if (field?.label) opt.label = field.label;
+      }
+    }
+  }
+}
