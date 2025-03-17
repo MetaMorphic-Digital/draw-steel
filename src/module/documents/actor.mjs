c2e6abb96f51da67244a66025a5bb1a4c2ff2051
Kody Verrett <kody.verrett@proton.me>
2025-03-17 13:19:07 -0500

    Fix token hud not allowing negative stamina and adjust token bar for negative stamina

--- src/module/documents/actor.mjs
+++ src/module/documents/actor.mjs
@@ -34,4 +34,28 @@ export default class DrawSteelActor extends Actor {
     if (this.system.rollCharacteristic instanceof Function) return this.system.rollCharacteristic(characteristic, options);
     throw new Error(`Actors of type ${this.type} cannot roll characteristics`);
   }
+
+  /**
+   * Handle how changes to a Token attribute bar are applied to the Actor.
+   * This allows for game systems to override this behavior and deploy special logic.
+   * @param {string} attribute    The attribute path
+   * @param {number} value        The target attribute value
+   * @param {boolean} isDelta     Whether the number represents a relative change (true) or an absolute change (false)
+   * @param {boolean} isBar       Whether the new value is part of an attribute bar, or just a direct value
+   * @returns {Promise<documents.Actor>}  The updated Actor document
+   */
+  async modifyTokenAttribute(attribute, value, isDelta = false, isBar = true) {
+    if (attribute !== "stamina") return super.modifyTokenAttribute(attribute, value, isDelta, isBar);
+
+    const current = this.system.stamina.value;
+    const update = isDelta ? current + value : value;
+    if (update === current) return this;
+
+    // Determine the updates to make to the actor data
+    const updates = { "system.stamina.value": Math.clamp(update, -this.system.stamina.winded, this.system.stamina.max) };
+
+    // Allow a hook to override these changes
+    const allowed = Hooks.call("modifyTokenAttribute", { attribute, value, isDelta, isBar }, updates, this);
+    return allowed !== false ? this.update(updates) : this;
+  }
 }
