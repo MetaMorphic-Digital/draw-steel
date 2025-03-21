90ca46ee60ba328d913677b75ab80b0d4c0c2808
Kody Verrett <kody.verrett@proton.me>
2025-03-20 23:22:14 -0500

    Update chat message hook (#333)

--- src/module/applications/hooks/chatMessageHTML.mjs
+++ src/module/applications/hooks/chatMessageHTML.mjs
@@ -3,12 +3,11 @@ import DrawSteelChatMessage from "../../documents/chat-message.mjs";
 /**
  * A hook event that fires for each ChatMessage which is rendered for addition to the ChatLog.
  * This hook allows for final customization of the message HTML before it is added to the log.
- * // TODO: In v13, adjust the hook call and the second parameter
  * @param {DrawSteelChatMessage} message The ChatMessage document being rendered
- * @param {JQuery<HTMLLIElement>} jquery The pending HTML as a jQuery object
+ * @param {HTMLLIElement} html The pending HTML
  * @param {Record<string, any>} context
  */
-export async function renderChatMessage(message, [html], context) {
+export async function renderChatMessageHTML(message, html, context) {
   if (message.system.alterMessageHTML instanceof Function) {
     await message.system.alterMessageHTML(html);
   }
