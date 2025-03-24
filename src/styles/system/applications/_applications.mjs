8cae92f8fd731d944ff1536cefa0e9b78522b8ba
Kody Verrett <kody.verrett@proton.me>
2025-03-24 11:39:01 -0500

    move applications styles to system folder to align with top level folders being layers

---
+++ src/styles/system/applications/_applications.mjs
@@ -0,0 +1,16 @@
+import "./apps/monster-metadata.css";
+import "./apps/power-roll-dialog.css";
+import "./apps/targeted-condition-prompt.css";
+
+import "./components/effects.css";
+import "./components/embeds.css";
+import "./components/forms.css";
+import "./components/items.css";
+
+import "./sheets/active-effect-config.css";
+import "./sheets/actor-sheet.css";
+import "./sheets/combatant-group-config.css";
+import "./sheets/item-sheet.css";
+
+import "./sidebar/tabs/chat.css";
+import "./sidebar/tabs/combat.css";
