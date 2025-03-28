c1f2791a72eed8b7347b978b927f0d352b1b3931
Joseph <JPMeehan5@Gmail.com>
2025-03-27 17:30:45 -0700

    Added canvas to global fix

--- draw-steel.d.ts
+++ draw-steel.d.ts
@@ -1,3 +1,4 @@
+import Canvas from "@client/canvas/board.mjs";
 import "./src/module/_types";
 import "@client/global.mjs";
 
@@ -13,4 +14,8 @@ declare global {
   class Hooks extends foundry.helpers.Hooks {}
   const fromUuid = foundry.utils.fromUuid;
   const fromUuidSync = foundry.utils.fromUuidSync;
+  /**
+   * The singleton game canvas
+   */
+  const canvas: Canvas;
 }
