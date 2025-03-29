9b075b058ee3a4d737a17618c8210b99d0b1f109
Joseph <JPMeehan5@Gmail.com>
2025-03-29 16:22:15 -0700

    Combat group permissions (#355)
    * Type and system minimum updates
    
    * Fixed false interactivity for players
    
    * Updated changelog
    
    * Added canvas to global fix

--- draw-steel.d.ts
+++ draw-steel.d.ts
@@ -1,36 +1,10 @@
-import Game from "@client/game.mjs";
+import Canvas from "@client/canvas/board.mjs";
 import "./src/module/_types";
-import Application from "@client/appv1/api/application-v1.mjs";
-import ApplicationV2 from "@client/applications/api/application.mjs";
-import SceneControls from "@client/applications/ui/scene-controls.mjs";
-import Hotbar from "@client/applications/ui/hotbar.mjs";
-import MainMenu from "@client/applications/ui/main-menu.mjs";
-import SceneNavigation from "@client/applications/ui/scene-navigation.mjs";
-import Notifications from "@client/applications/ui/notifications.mjs";
-import GamePause from "@client/applications/ui/game-pause.mjs";
-import Players from "@client/applications/ui/players.mjs";
-import Sidebar from "@client/applications/sidebar/sidebar.mjs";
-import createConfig from "@client/config.mjs";
+import "@client/global.mjs";
 
 // Foundry's use of `Object.assign(globalThis) means many globally available objects are not read as such
 // This declare global hopefully fixes that
 declare global {
-  readonly const game: Game;
-
-  readonly const ui: {
-    activeWindow: Application|ApplicationV2,
-    controls: SceneControls,
-    hotbar: Hotbar,
-    menu: MainMenu,
-    nav: SceneNavigation,
-    notifications: Notifications,
-    pause: GamePause,
-    players: Players,
-    sidebar: Sidebar,
-    windows: Record<string, Application>
-    // it's possible we want to expand this to all actually-used options in CONFIG.ui
-  }
-
   // not a real extension of course but simplest way for this to work with the intellisense.
   /**
    * A simple event framework used throughout Foundry Virtual Tabletop.
@@ -40,4 +14,8 @@ declare global {
   class Hooks extends foundry.helpers.Hooks {}
   const fromUuid = foundry.utils.fromUuid;
   const fromUuidSync = foundry.utils.fromUuidSync;
+  /**
+   * The singleton game canvas
+   */
+  const canvas: Canvas;
 }
