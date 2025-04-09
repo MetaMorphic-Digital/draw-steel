deede197c21a79179a6525e23017e75941b354cc
Joseph <JPMeehan5@Gmail.com>
2025-04-08 19:15:01 -0700

    Improved ability parsing

--- src/module/data/item/ability.mjs
+++ src/module/data/item/ability.mjs
@@ -345,12 +345,17 @@ export default class AbilityModel extends BaseItemModel {
 
     context.characteristics = Object.entries(ds.CONFIG.characteristics).map(([value, { label }]) => ({ value, label }));
 
-    const characteristicsFormatter = game.i18n.getListFormatter({ type: "disjunction" });
-    const characteristicList = this.powerRoll.characteristics.map(characteristic => {
-      const localizedCharacteristic = ds.CONFIG.characteristics[characteristic]?.label ?? characteristic;
-      return (characteristic === this.powerRoll.characteristic) ? `<em>${localizedCharacteristic}</em>` : localizedCharacteristic;
-    });
-    context.powerRollCharacteristicsList = characteristicsFormatter.format(Array.from(characteristicList));
+    context.powerRollBonus = this.powerRoll.formula;
+
+    if (this.powerRoll.formula.includes("@chr")) {
+      const characteristicsFormatter = game.i18n.getListFormatter({ type: "disjunction" });
+      const characteristicList = this.powerRoll.characteristics.map(characteristic => {
+        const localizedCharacteristic = ds.CONFIG.characteristics[characteristic]?.label ?? characteristic;
+        return (characteristic === this.powerRoll.characteristic) ? `<em>${localizedCharacteristic}</em>` : localizedCharacteristic;
+      });
+
+      context.powerRollBonus = this.powerRoll.formula.replace("@chr", characteristicsFormatter.format(Array.from(characteristicList)));
+    }
 
     context.powerRollEffectOptions = Object.entries(this.schema.fields.powerRoll.fields.tier1.element.types).map(([value, { label }]) => ({ value, label }));
 
