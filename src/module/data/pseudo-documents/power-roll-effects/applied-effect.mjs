1a41ac749cd71c965e826e4fcd542d2e7ef6f4b7
Joseph <JPMeehan5@Gmail.com>
2025-05-14 09:00:19 -0700

    Apply suggestions from code review
    Co-authored-by: Zhell <50169243+krbz999@users.noreply.github.com>

--- src/module/data/pseudo-documents/power-roll-effects/applied-effect.mjs
+++ src/module/data/pseudo-documents/power-roll-effects/applied-effect.mjs
@@ -110,6 +110,7 @@ export default class AppliedPowerRollEffect extends BasePowerRollEffect {
       };
     }
   }
+
   /* -------------------------------------------------- */
 
   /** @inheritdoc */
