435bcfcff5b624459aa5dc36a75c96365d5d91f9
Kody Verrett <kody.verrett@proton.me>
2025-05-16 12:58:49 -0500

    remove all slide-toggle related code

--- src/module/applications/elements/_module.mjs
+++
@@ -1,3 +0,0 @@
-export { default as AdoptedStyleSheetMixin } from "./adopted-stylesheet-mixin.mjs";
-export { default as CheckboxElement } from "./checkbox.mjs";
-export { default as SlideToggleElement } from "./slide-toggle.mjs";
--- src/styles/elements/_elements.mjs
+++ src/styles/elements/_elements.mjs
@@ -1,2 +1,1 @@
 import "./typography.css";
-import "./slide-toggle.css";
