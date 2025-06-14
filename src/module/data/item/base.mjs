ae171d84190984debfae8a2b310281b01f74ff0c
Joseph <JPMeehan5@Gmail.com>
2025-06-14 06:19:24 -0700

    Fixed item creation (#593)

--- src/module/data/item/base.mjs
+++ src/module/data/item/base.mjs
@@ -37,7 +37,7 @@ export default class BaseItemModel extends SubtypeModelMixin(foundry.abstract.Ty
      * The Draw Steel ID, indicating a unique game rules element
      * @remarks `readonly: true` makes this non-iterable
      */
-    schema._dsid = new fields.StringField({ blank: false, readonly: true });
+    schema._dsid = new fields.StringField({ required: true, readonly: true });
 
     return schema;
   }
