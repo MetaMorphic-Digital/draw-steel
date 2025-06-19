bee14bacfc2c9101e69c05ec0f800ebbece33bea
Joseph <JPMeehan5@Gmail.com>
2025-06-19 14:35:20 -0700

    Wired up effect end select persistence

--- src/module/applications/hud/token-hud.mjs
+++ src/module/applications/hud/token-hud.mjs
@@ -10,16 +10,30 @@ export default class DrawSteelTokenHUD extends foundry.applications.hud.TokenHUD
     },
   };
 
+  /**
+   * Current option for the select, not stored in the database and shared between all tokens.
+   */
+  effectEnd = "";
+
   /** @inheritdoc */
   async _onRender(context, options) {
     await super._onRender(context, options);
 
-    context.dsEffectEnds = ds.CONFIG.effectEnds;
+    context.dsEffectEnds = {
+      selected: this.effectEnd,
+      options: ds.CONFIG.effectEnds,
+    };
 
     const effectPalette = this.element.querySelector("div[data-palette=\"effects\"]");
 
     const paletteContents = await foundry.applications.handlebars.renderTemplate(systemPath("templates/hud/effect-palette.hbs"), context);
 
     effectPalette.innerHTML = paletteContents;
+
+    const effectEndSelect = effectPalette.querySelector("[data-name=\"system.end.type\"]");
+
+    effectEndSelect.addEventListener("change", (ev) => {
+      this.effectEnd = effectEndSelect.value;
+    });
   }
 }
