c576ef0e0e40b3ca8cbcae61c6aed40499ab8537
Joseph <JPMeehan5@Gmail.com>
2025-06-19 16:57:51 -0700

    Improved display and application of statuses in pane (#620)
    * Iterated on alternative effect palette
    
    * Updated changelog
    
    * Refined effect palette display
    
    * Wired up effect end select persistence
    
    * Wired up toggle effect logic
    
    * CSS cleanup

--- src/module/applications/_module.mjs
+++ src/module/applications/_module.mjs
@@ -1,6 +1,7 @@
 export * as api from "./api/_module.mjs";
 export * as apps from "./apps/_module.mjs";
 export * as hooks from "./hooks/_module.mjs";
+export * as hud from "./hud/_module.mjs";
 export * as sheets from "./sheets/_module.mjs";
 export * as ui from "./ui/_module.mjs";
 export * as ux from "./ux/_module.mjs";
