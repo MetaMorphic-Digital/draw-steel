4bf152448954ea403d33a52b0d2c0cceae49fd6c
Joseph <JPMeehan5@Gmail.com>
2025-08-15 12:51:00 -0700

    Initial draft of migration

--- src/module/data/migrations.mjs
+++ src/module/data/migrations.mjs
@@ -1,6 +1,11 @@
 import { systemID } from "../constants.mjs";
 
 /**
+ * @import DocumentCollection from "@client/documents/abstract/document-collection.mjs";
+ * @import {Document, EmbeddedCollection} from "@common/abstract/_module.mjs";
+ */
+
+/**
  * Perform one-time migrations
  * Run and awaited in the `ready` hook before `ds.ready` is called.
  */
@@ -15,4 +20,26 @@ export async function migrateWorld() {
     await game.users.activeGM.update({ name: game.i18n.localize("USER.RoleGamemaster") });
     await game.settings.set(systemID, "migrationVersion", game.system.version);
   }
+  else if (foundry.utils.isNewerVersion("0.8.0", migrationVersion)) {
+    await migrateType(game.actors);
+    await migrateType(game.items);
+    // await game.settings.set(systemID, "migrationVersion", game.system.version);
+  }
+}
+
+/**
+ * Migrate the types of documents in the collection.
+ * @param {DocumentCollection<Document> | EmbeddedCollection<Document>} collection
+ * @param {object} [options={}]       Options forwarded to the document update operation.
+ * @param {string} [options.pack]     Pack to update.
+ * @param {Document} [options.parent] Parent of the collection for embedded collections.
+ */
+export async function migrateType(collection, options = {}) {
+  const toMigrate = collection.filter(doc => doc.getFlag(systemID, "migrateType")).map(doc => ({
+    _id: doc.id,
+    "==system": doc.system.toObject(),
+    "flags.draw-steel.-=migrateType": null,
+  }));
+  console.log(collection, toMigrate);
+  // collection.documentClass.updateDocuments(toMigrate, { pack: options.pack, parent: options.parent });
 }
