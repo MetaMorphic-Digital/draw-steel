59bd114871a9ad6b2bd1d50aaf56358763d72d15
Joseph <JPMeehan5@Gmail.com>
2025-08-21 22:29:48 -0700

    Adjusted item sheet minimum width, moved locations of feature items

--- src/module/applications/sheets/item-sheet.mjs
+++ src/module/applications/sheets/item-sheet.mjs
@@ -19,9 +19,9 @@ export default class DrawSteelItemSheet extends DSDocumentSheetMixin(sheets.Item
   static DEFAULT_OPTIONS = {
     classes: ["item"],
     position: {
-      // Allows "Their Lack of Focus is Their Undoing" to fit in two lines
+      // Allows "Allow Me to Introduce Tonight’s Players" to fit in two lines
       // Also ensures the prosemirror editor bar doesn't overflow to a second line when selecting a paragraph element
-      width: 560,
+      width: 580,
     },
     actions: {
       toggleMode: this.#toggleMode,
