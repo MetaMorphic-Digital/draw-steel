0c9efb3523bb2e75d9ea8878cfbc58645e628527
Justin Campbell <justinpcampbell@gmail.com>
2025-11-04 11:28:48 -0800

    Fix "hr" alias on the /gain enricher

--- src/module/applications/ux/enrichers/roll.mjs
+++ src/module/applications/ux/enrichers/roll.mjs
@@ -286,7 +286,9 @@ function enrichGain(parsedConfig, label, options) {
   let resourceType;
 
   switch (linkConfig.gainType) {
-    case "hr": // eslint-ignore no-fallthrough
+    case "hr":
+      // Rewrite the "hr" alias with the full value so that the added dataset is consistent
+      linkConfig.gainType = "heroic"; // eslint-ignore no-fallthrough
     case "heroic":
       resourceType = game.i18n.localize("DRAW_STEEL.Actor.hero.FIELDS.hero.primary.value.label");
       break;
