2acad246bee6f44889fa5f47715e4a6f5e273244
Joseph <JPMeehan5@Gmail.com>
2025-11-25 22:03:30 -0800

    Building Summoning API

---
+++ src/module/canvas/layers/tokens.mjs
@@ -0,0 +1,5 @@
+
+/**
+ * A Placeable Layer subclass adding system-specific behavior and registered in CONFIG.Canvas.layers.tokens.layerClass.
+ */
+export default class DrawSteelTokenLayer extends foundry.canvas.layers.TokenLayer {}
