2dd0ae876397acbc54b622adae43884d218c034e
Andrew Schlachter <DocSchlock@users.noreply.github.com>
2025-12-05 16:34:41 -0500

    change to label

--- src/module/applications/sheets/actor-sheet.mjs
+++ src/module/applications/sheets/actor-sheet.mjs
@@ -272,7 +272,7 @@ export default class DrawSteelActorSheet extends DSDocumentSheet {
     }
     return {
       list: formatter.format(languageList),
-      options: languageOptions.sort((a,b) => a.value.localeCompare(b.value)),
+      options: languageOptions.sort((a,b) => a.label.localeCompare(b.label)),
     };
   }
 
