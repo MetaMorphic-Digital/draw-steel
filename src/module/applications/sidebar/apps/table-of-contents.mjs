e3b83c856a41c180ed277a550f0aacec3cd8cc25
Joseph <JPMeehan5@Gmail.com>
2025-12-19 10:54:19 -0800

    Basic context and part rendering

--- src/module/applications/sidebar/apps/table-of-contents.mjs
+++ src/module/applications/sidebar/apps/table-of-contents.mjs
@@ -1,11 +1,18 @@
+import { systemID, systemPath } from "../../../constants.mjs";
+
 export default class DrawSteelCompendiumTOC extends foundry.applications.sidebar.apps.Compendium {
   /** @inheritdoc */
   static DEFAULT_OPTIONS = {
-    classes: ["draw-steel", "table-of-contents"],
     actions: {
       configureTOC: this.#configureTOC,
     },
+    classes: ["draw-steel", "table-of-contents"],
+    position: {
+      width: 800,
+      height: 950,
+    },
     window: {
+      contentTag: "article",
       controls: [
         {
           action: "configureTOC",
@@ -14,8 +21,111 @@ export default class DrawSteelCompendiumTOC extends foundry.applications.sidebar
           visible: this.#canConfigureTOC,
         },
       ],
+      resizable: true,
     },
   };
+
+  /* -------------------------------------------------- */
+
+  /** @inheritdoc */
+  static PARTS = {
+    header: {
+      template: systemPath("templates/sidebar/apps/table-of-contents/header.hbs"),
+      templates: [],
+    },
+    contents: {
+      template: systemPath("templates/sidebar/apps/table-of-contents/contents.hbs"),
+      templates: [],
+    },
+  };
+
+  /* -------------------------------------------------- */
+
+  /** @inheritdoc */
+  _createContextMenus() {
+    this._createContextMenu(this._getEntryContextOptions, "[data-document-id][data-entry-id]", {
+      fixed: true,
+      parentClassHooks: false,
+      hookName: "getJournalEntryContextOptions",
+    });
+  }
+
+  /* -------------------------------------------------- */
+
+  /** @inheritdoc */
+  _configureRenderParts(options) {
+    // the super call assumes a `directory` part we do not use
+    return foundry.utils.deepClone(this.constructor.PARTS);
+  }
+
+  /* -------------------------------------------------- */
+
+  /** @inheritDoc */
+  async _prepareContext(options) {
+    const context = await super._prepareContext(options);
+    const documents = await this.collection.getDocuments();
+
+    context.chapters = [];
+    const specialEntries = [];
+    for (const entry of documents) {
+      const flags = entry.flags?.[systemID];
+      if (!flags) continue;
+      const keys = Object.keys(flags);
+      if (flags.tocHidden || !keys.length || ((keys.length === 1) && (keys[0] === "navigation"))) continue;
+      const type = flags.type ?? "chapter";
+
+      if (type === "header") {
+        const page = entry.pages.contents[0];
+        context.header = {
+          title: flags.title ?? page?.name,
+          content: page?.text.content,
+        };
+        continue;
+      }
+
+      const data = {
+        type, flags,
+        id: entry.id,
+        name: flags.title ?? entry.name,
+        pages: Array.from(entry.pages).map(({ flags, id, name, sort }) => ({
+          id, sort, flags,
+          name: flags[systemID]?.title ?? name,
+          entryId: entry.id,
+        })),
+      };
+
+      if (type === "special") {
+        data.showPages = flags.showPages ?? !flags.append;
+        specialEntries.push(data);
+      } else {
+        data.order = (this.constructor.TYPES[type] ?? 200) + (flags.position ?? 0);
+        data.showPages = (flags.showPages !== false) && ((flags.showPages === true) || (type === "chapter"));
+        context.chapters.push(data);
+      }
+    }
+
+    context.chapters.sort((lhs, rhs) => lhs.order - rhs.order);
+    for (const entry of specialEntries) {
+      const append = entry.flags.append;
+      const order = entry.flags.order;
+      if (append && (append <= context.chapters.length)) {
+        context.chapters[append - 1].pages.push({ ...entry, sort: order, entry: true });
+      } else {
+        context.chapters.push(entry);
+      }
+    }
+
+    for (const chapter of context.chapters) {
+      chapter.pages = chapter.pages
+        .filter(p => !p.flags.tocHidden && (chapter.showPages || p.entry))
+        .sort((lhs, rhs) => lhs.sort - rhs.sort);
+      for (const page of chapter.pages) {
+        if (page.pages) page.pages.sort((lhs, rhs) => lhs.sort - rhs.sort);
+      }
+    }
+
+    return context;
+  }
 
   /* -------------------------------------------------- */
   /*   Actions                                          */
@@ -53,5 +163,17 @@ export default class DrawSteelCompendiumTOC extends foundry.applications.sidebar
    */
   static async #configureTOC(event, target) {
     console.log(this, event, target);
+  }
+
+  /* -------------------------------------------------- */
+
+  /** @inheritdoc */
+  async _onClickEntry(event, target) {
+    const entryId = target.closest("[data-entry-id]")?.dataset.entryId;
+    if (!entryId) return;
+    const entry = await this.collection.getDocument(entryId);
+    entry?.sheet.render(true, {
+      pageId: target.closest("[data-page-id]")?.dataset.pageId,
+    });
   }
 }
