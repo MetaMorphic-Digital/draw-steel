b9be8ea1ea89d6f6e7206b3f55e0362356330b3b
Joseph <JPMeehan5@Gmail.com>
2025-12-19 16:31:02 -0800

    Started working on TOC Config app

--- src/module/applications/sidebar/apps/table-of-contents.mjs
+++ src/module/applications/sidebar/apps/table-of-contents.mjs
@@ -1,6 +1,22 @@
 import { systemID, systemPath } from "../../../constants.mjs";
+import CompendiumTOCConfig from "../../apps/toc-config.mjs";
 
+/**
+ * An opt-in compendium display that renders a Journal Entry compendium as a Table of Contents.
+ */
 export default class DrawSteelCompendiumTOC extends foundry.applications.sidebar.apps.Compendium {
+  /**
+   * Helper function to replace the application class of Journal compendiums with this.
+   */
+  static applyToPacks() {
+    for (const pack of game.packs.filter(p => p.metadata.type === "JournalEntry")) {
+      pack.applicationClass = this;
+      // if (pack.metadata.flags.display === "table-of-contents") pack.applicationClass = this;
+    }
+  }
+
+  /* -------------------------------------------------- */
+
   /** @inheritdoc */
   static DEFAULT_OPTIONS = {
     actions: {
@@ -17,7 +33,7 @@ export default class DrawSteelCompendiumTOC extends foundry.applications.sidebar
         {
           action: "configureTOC",
           icon: "fa-solid fa-edit",
-          label: "DRAW_STEEL.COMPENDIUM.TOC.configure",
+          label: "DRAW_STEEL.COMPENDIUM.TOC.configure.action",
           visible: this.#canConfigureTOC,
         },
       ],
@@ -137,18 +153,6 @@ export default class DrawSteelCompendiumTOC extends foundry.applications.sidebar
   /* -------------------------------------------------- */
 
   /**
-   * Helper function to replace the application class of Journal compendiums with a TOC view.
-   */
-  static applyToPacks() {
-    for (const pack of game.packs.filter(p => p.metadata.type === "JournalEntry")) {
-      pack.applicationClass = this;
-      // if (pack.metadata.flags.display === "table-of-contents") pack.applicationClass = this;
-    }
-  }
-
-  /* -------------------------------------------------- */
-
-  /**
    * Include "Configure TOC" in the window controls.
    * @this DrawSteelCompendiumTOC
    */
@@ -167,7 +171,9 @@ export default class DrawSteelCompendiumTOC extends foundry.applications.sidebar
    * @private
    */
   static async #configureTOC(event, target) {
-    console.log(this, event, target);
+    const fd = await CompendiumTOCConfig.create({ compendium: this.collection });
+
+    getDocumentClass("JournalEntry").updateDocuments(fd, { pack: this.collection.collection });
   }
 
   /* -------------------------------------------------- */
