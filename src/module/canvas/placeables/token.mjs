2786b058e0f2176e9989bf53a1008b3379153bd2
Kody Verrett <kody.verrett@proton.me>
2026-01-04 15:19:58 -0700

    Add AE targetable path for canFlank

--- src/module/canvas/placeables/token.mjs
+++ src/module/canvas/placeables/token.mjs
@@ -28,9 +28,11 @@ export default class DrawSteelToken extends foundry.canvas.placeables.Token {
    * @type {boolean}
    */
   get canFlank() {
-    if (!this.actor) return true;
-    // Dead actors cannot flank
-    if (this.actor.statuses.has("dead")) return false;
+    if (!this.actor) return false;
+    // Defeated/Dead actors cannot flank
+    if (this.actor.statuses.has(CONFIG.specialStatusEffects.DEFEATED)) return false;
+    // Check if active effects have modified the ability to flank
+    if (!this.actor.system.statuses.canFlank) return false;
     // Checking if active effects have restricted triggered abilities
     return !this.actor.system.restrictions.type.has("triggered");
   }
