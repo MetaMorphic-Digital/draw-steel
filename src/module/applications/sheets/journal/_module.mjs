e0064db86c2f535fef7e9d64f35c7d8f90e2cb60
Joseph <JPMeehan5@Gmail.com>
2026-02-02 22:31:27 -0800

    [#1540] Power Roll Result page (#1582)
    * Data model work
    
    * Page construction, embed fixes
    
    * Created proper page view for header purposes
    
    * Fixed eslint indentation
    
    * More indentation fix

--- src/module/applications/sheets/journal/_module.mjs
+++ src/module/applications/sheets/journal/_module.mjs
@@ -1,3 +1,4 @@
 export { default as ConfigPage } from "./config-page.mjs";
 export { default as DrawSteelImageSheet } from "./draw-steel-image-sheet.mjs";
 export { default as ReferencePage } from "./reference-page.mjs";
+export { default as TierOutcomePage } from "./tier-outcome-page.mjs";
