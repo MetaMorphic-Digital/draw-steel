b01fc07e50631b7e264e48d0bd7563faff6a7085
Joseph <JPMeehan5@Gmail.com>
2026-02-08 10:30:34 -0800

    HTML and listener construction

--- src/module/documents/scene.mjs
+++ src/module/documents/scene.mjs
@@ -4,11 +4,47 @@
 export default class DrawSteelScene extends foundry.documents.Scene {
   /** @inheritdoc */
   async _buildEmbedHTML(config, options = {}) {
-    if (!config.hideThumb && this.thumb) {
+    // Ignore normal cite and caption logic
+    const embed = document.createElement("document-embed");
+
+    const label = config.cite ? this.toAnchor({ name: config.label }) : foundry.utils.parseHTML(`<span>${config.label || this.name}</span>`);
+
+    const viewButton = document.createElement("a");
+    viewButton.append(foundry.applications.fields.createFontAwesomeIcon("fa-eye"));
+    viewButton.dataset.tooltip = "SCENE.View";
+    viewButton.dataset.embedAction = "view";
+
+    const activateButton = document.createElement("a");
+    activateButton.append(foundry.applications.fields.createFontAwesomeIcon("fa-bullseye"));
+    activateButton.dataset.tooltip = "SCENE.Activate";
+    viewButton.dataset.embedAction = "activate";
+
+    if (config.inline || !this.thumb) {
+      embed.append(label);
+      if (game.user.isGM) embed.append(viewButton, activateButton);
+    } else {
+      const figure = document.createElement("figure");
+
+      const figCaption = document.createElement("figcaption");
+      figCaption.append(label);
+      if (game.user.isGM) figCaption.append(viewButton, activateButton);
+
       const img = document.createElement("img");
       img.src = this.thumb;
 
-      return img;
+      figure.append(figCaption, img);
+
+      embed.append(figure);
     }
+
+    return embed;
+  }
+
+  /* -------------------------------------------------- */
+
+  /** @inheritdoc */
+  onEmbed(element) {
+    element.querySelector("a[data-embed-action=\"view\"]")?.addEventListener("click", () => this.view());
+    element.querySelector("a[data-embed-action=\"activate\"]")?.addEventListener("click", () => this.activate());
   }
 }
