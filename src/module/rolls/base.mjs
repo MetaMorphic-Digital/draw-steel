c00b10cf53a5ec0f4b2a66090f3aa1080e74c10a
Zhell <kennethzhell@gmail.com>
2026-02-23 19:54:24 +0100

    [#1446] Implement `toMessage` to leverage message parts

--- src/module/rolls/base.mjs
+++ src/module/rolls/base.mjs
@@ -2,4 +2,45 @@
 /**
  * Base roll class for Draw Steel.
  */
-export default class DSRoll extends foundry.dice.Roll { }
+export default class DSRoll extends foundry.dice.Roll {
+  /**
+   * The type used for the `part` of a standard message.
+   * @type {string}
+   */
+  static PART_TYPE = "";
+
+  /* -------------------------------------------------- */
+
+  /** @inheritdoc */
+  async toMessage(messageData = {}, { rollMode, create = true } = {}) {
+    if (!this.constructor.PART_TYPE) return super.toMessage(messageData, { rollMode, create });
+
+    if (rollMode === "roll") rollMode = undefined;
+    rollMode ||= game.settings.get("core", "rollMode");
+
+    if (!this._evaluated) await this.evaluate({ allowInteractive: rollMode !== CONST.DICE_ROLL_MODES.BLIND });
+
+    messageData = foundry.utils.mergeObject({
+      type: "standard",
+      system: {},
+      author: game.user.id,
+      sound: CONFIG.sounds.dice,
+    }, messageData);
+    messageData.system.parts = {
+      [foundry.utils.randomID()]: {
+        flavor: messageData.flavor,
+        rolls: [this],
+        type: this.constructor.PART_TYPE,
+      },
+    };
+    delete messageData.flavor;
+    delete messageData.rolls;
+
+    const Cls = getDocumentClass("ChatMessage");
+    const msg = new Cls(messageData);
+    msg.applyRollMode(rollMode);
+
+    if (create) return Cls.create(msg);
+    return msg.toObject();
+  }
+}
