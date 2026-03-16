a9189304a809ab15f14f94db846a9ef7404b4603
Kody Verrett <kody.verrett@proton.me>
2026-03-16 15:34:40 -0600

    AE mode to type changes

--- src/module/data/item/ability.mjs
+++ src/module/data/item/ability.mjs
@@ -182,7 +182,7 @@ export default class AbilityModel extends BaseItemModel {
     // Apply keyword modifiers first to ensure later effects operate on the modified set
     for (const bonus of (this.actor.system._abilityBonuses ?? [])) {
       if (bonus.key !== "keyword") continue;
-      if (bonus.mode !== CONST.ACTIVE_EFFECT_MODES.ADD) continue;
+      if (bonus.type !== "add") continue;
       if (!bonus.filters.keywords.isSubsetOf(this.keywords)) continue;
 
       this.keywords.add(bonus.value);
