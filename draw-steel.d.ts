003a9debc0e56c6b6b54edd22421215e3dc8e92d
Joseph <JPMeehan5@Gmail.com>
2026-03-19 13:33:42 -0700

    Type updates/adjustments

--- draw-steel.d.ts
+++ draw-steel.d.ts
@@ -3,26 +3,25 @@ import "@client/global.mjs";
 import "@common/global.mjs";
 import "@common/primitives/global.mjs";
 import Canvas from "@client/canvas/board.mjs";
+import DrawSteelTokenLayer from "./src/module/canvas/layers/tokens.mjs";
 
 // Foundry's use of `Object.assign(globalThis) means many globally available objects are not read as such
 // This declare global hopefully fixes that
 declare global {
-  // not a real extension of course but simplest way for this to work with the intellisense.
-  /**
-   * A simple event framework used throughout Foundry Virtual Tabletop.
-   * When key actions or events occur, a "hook" is defined where user-defined callback functions can execute.
-   * This class manages the registration and execution of hooked callback functions.
-   */
-  class Hooks extends foundry.helpers.Hooks {}
-  /**
-   * A representation of a color in hexadecimal format.
-   * This class provides methods for transformations and manipulations of colors.
-   */
-  class Color extends foundry.utils.Color {}
-  const fromUuid = foundry.utils.fromUuid;
-  const fromUuidSync = foundry.utils.fromUuidSync;
-  /**
-   * The singleton game canvas.
-   */
-  const canvas: Canvas;
+  namespace globalThis {
+    // not a real extension of course but simplest way for this to work with the intellisense.
+    /**
+     * A simple event framework used throughout Foundry Virtual Tabletop.
+     * When key actions or events occur, a "hook" is defined where user-defined callback functions can execute.
+     * This class manages the registration and execution of hooked callback functions.
+     */
+    class Hooks extends foundry.helpers.Hooks {}
+    /**
+     * A representation of a color in hexadecimal format.
+     * This class provides methods for transformations and manipulations of colors.
+     */
+    class Color extends foundry.utils.Color {}
+    const fromUuid = foundry.utils.fromUuid;
+    const fromUuidSync = foundry.utils.fromUuidSync;
+  }
 }
--- src/module/canvas/_types.d.ts
+++ src/module/canvas/_types.d.ts
@@ -1,5 +1,21 @@
 import "./placeables/tokens/_types";
 import DrawSteelTokenDocument from "../documents/token.mjs";
+import DrawSteelTokenLayer from "./layers/tokens.mjs";
+
+declare module "@client/canvas/board.mjs" {
+  export default interface Canvas {
+    controls: foundry.canvas.layers.ControlsLayer;
+    drawings: foundry.canvas.layers.DrawingsLayer;
+    grid: foundry.canvas.layers.GridLayer;
+    lighting: foundry.canvas.layers.LightingLayer;
+    notes: foundry.canvas.layers.NotesLayer;
+    regions: foundry.canvas.layers.RegionLayer;
+    sounds: foundry.canvas.layers.SoundsLayer;
+    tiles: foundry.canvas.layers.TilesLayer;
+    tokens: DrawSteelTokenLayer;
+    walls: foundry.canvas.layers.WallsLayer;
+  }
+}
 
 declare module "@client/canvas/placeables/token.mjs" {
   export default interface Token {
