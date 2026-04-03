99567a68bb94f4dcafb6a6baaf40d89de395c242
Joseph <JPMeehan5@Gmail.com>
2026-04-03 13:50:42 -0700

    [#1799] Fixed Purifying Fire, sudoku edits (#1814)
    * [#1799] Fixed Purifying Fire, sudoku edits
    
    * Override to inheritdoc adjustments
    
    * Re-fixed uuid
    
    * Found data issue
    
    * Removed unneeded expand effect call

--- src/module/data/fields/lazy-typed-schema-field.mjs
+++ src/module/data/fields/lazy-typed-schema-field.mjs
@@ -6,29 +6,8 @@
 export default class LazyTypedSchemaField extends foundry.data.fields.TypedSchemaField {
   /** @inheritdoc */
   _validateSpecial(value) {
-    if (!value || (value.type in this.types)) return super._validateSpecial(value);
-    return true;
-  }
-
-  /* -------------------------------------------------- */
-
-  /** @override */
-  _updateCommit(source, key, value, diff, options) {
-    const s = source[key];
-
-    // Special Cases: * -> undefined, * -> null, undefined -> *, null -> *
-    if (!s || !value) {
-      source[key] = value;
-      return;
-    }
-
-    // Update fields in source which changed in the diff
-    const element = this.types[value.type];
-    for (let [k, d] of Object.entries(diff)) {
-      k = foundry.utils.isDeletionKey(k) ? k.slice(2) : k;
-      const field = element.getField(k);
-      if (!field) continue;
-      field._updateCommit(s, k, value[k], d, options);
-    }
+    const invalidType = (typeof value.type === "string") && !(value.type in this.types);
+    if (invalidType) return true;
+    return super._validateSpecial(value);
   }
 }
