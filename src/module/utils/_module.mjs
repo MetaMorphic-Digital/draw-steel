431e9dd38b10d01bca3b6b2ae030af2f12ee3178
Zhell <kennethzhell@gmail.com>
2026-05-29 17:39:40 +0200

    Add 'getDocumentTypes' util method

--- src/module/utils/_module.mjs
+++ src/module/utils/_module.mjs
@@ -2,6 +2,7 @@ export * as advancement from "./advancement/_module.mjs";
 export { default as constructHTMLButton } from "./construct-html-button.mjs";
 export { default as enrichHTML } from "./enrich-html.mjs";
 export { default as evaluateFormula } from "./evaluate-formula.mjs";
+export { default as getDocumentTypes } from "./get-document-types.mjs";
 export { default as MembersCollection } from "./members-collection.mjs";
 export { default as ModelCollection } from "./model-collection.mjs";
 export { default as simplifyRollFormula } from "./simplify-roll-formula.mjs";
