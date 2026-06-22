36904ab08ff2f3065ed13828583bc4ba75adfbf7
JPMeehan <jpmeehan5@gmail.com>
2026-06-21 21:47:43 -0400

    Basic Display

--- src/module/applications/apps/object-sizes-input.mjs
+++ src/module/applications/apps/object-sizes-input.mjs
@@ -8,6 +8,14 @@ export default class ObjectSizesInput extends DocumentInput {
   /** @inheritdoc */
   static DEFAULT_OPTIONS = {
     classes: ["object-sizes"],
+    actions: {
+      addShape: this.#addShape,
+      deleteShape: this.#deleteShape,
+      shapeRemoveAll: this.#shapeRemoveAll,
+    },
+    position: {
+      width: 500,
+    },
     window: {
       title: "DRAW_STEEL.Actor.object.ObjectSizes.DialogTitle",
       icon: "fa-solid fa-shapes",
@@ -20,6 +28,13 @@ export default class ObjectSizesInput extends DocumentInput {
   static PARTS = {
     body: {
       template: systemPath("templates/apps/document-input/object-sizes-input.hbs"),
+      templates: [
+        systemPath("templates/apps/object-sizes/circle.hbs"),
+        systemPath("templates/apps/object-sizes/emanation.hbs"),
+        systemPath("templates/apps/object-sizes/line.hbs"),
+        systemPath("templates/apps/object-sizes/rectangle.hbs"),
+        systemPath("templates/apps/object-sizes/ring.hbs"),
+      ],
     },
   };
 
@@ -28,15 +43,124 @@ export default class ObjectSizesInput extends DocumentInput {
   /** @inheritdoc */
   async _prepareContext(options) {
     const context = await super._prepareContext(options);
-    context.sizeSource = this.document.system.combat.size._source;
+    const sizeModel = this.document.system.combat.size;
+    context.sizeSource = sizeModel._source;
 
-    if ((context.sizeSource.shapes.length === 1) && !context.sizeSource.shapes[0].hole) {
-      const shape = context.sizeSource.shapes[0];
+    context.shapeContexts = sizeModel.shapes.map(shape => {
       const fields = { ...shape.schema.fields };
       delete fields.hole;
-      context.shapeContext = { rootId: this.id };
-      foundry.applications.apps.ShapeConfig._prepareShapeContext(context.shapeContext, shape, fields);
-    }
+      const shapeContext = { rootId: this.id };
+      foundry.applications.apps.ShapeConfig._prepareShapeContext(shapeContext, shape, fields);
+      return shapeContext;
+    });
+
     return context;
+  }
+
+  /* -------------------------------------------------- */
+
+  /**
+   * Add a new shape.
+   * @this ObjectSizesInput
+   * @param {PointerEvent} event   The originating click event.
+   * @param {HTMLElement} target   The capturing HTML element which defined a [data-action].
+   */
+  static async #addShape(event, target) {
+    const content = this.element.ownerDocument.createElement("div");
+
+    const { createFormGroup, createSelectInput } = foundry.applications.fields;
+
+    // curated from foundry.data.BaseShapeData.TYPES
+    const validShapes = [
+      "rectangle",
+      "circle",
+      "emanation",
+      "ring",
+      "line",
+    ];
+
+    content.append(createFormGroup({
+      label: "DRAW_STEEL.Actor.object.ObjectSizes.ShapeType",
+      input: createSelectInput({
+        options: validShapes.map(key => ({
+          value: key,
+          label: _loc(`SHAPE.TYPES.${key}.name`),
+        })),
+        name: "type",
+        sort: true,
+      }),
+      localize: true,
+    }));
+
+    const fd = await ds.applications.api.DSDialog.input({
+      content,
+      window: {
+        title: "DRAW_STEEL.Actor.object.ObjectSizes.AddShape",
+        icon: "fa-solid fa-shapes",
+      },
+    });
+
+    const objectSize = this.document.system.combat.size;
+
+    const shapeData = {
+      type: fd.type,
+      x: 0,
+      y: 0,
+      gridBased: true,
+    };
+
+    switch (fd.type) {
+      case "rectangle":
+        shapeData.width = shapeData.height = 1;
+        break;
+      case "circle":
+        shapeData.radius = 1;
+        break;
+      case "ellipse":
+        shapeData.radiusX = shapeData.radiusY = 1;
+        break;
+      case "emanation":
+        shapeData.radius = 1;
+        shapeData.base = {
+          type: "token",
+          width: objectSize.value,
+          height: objectSize.value,
+        };
+        break;
+      case "ring":
+        shapeData.radius = shapeData.innerWidth = shapeData.outerWidth = 1;
+        break;
+      case "line":
+        shapeData.length = shapeData.width = 1;
+        break;
+    }
+
+    await this.document.update({ "system.combat.size.shapes": objectSize._source.shapes.concat(shapeData) });
+  }
+
+  /* -------------------------------------------------- */
+
+  /**
+   * Delete a shape.
+   * @this ObjectSizesInput
+   * @param {PointerEvent} event   The originating click event.
+   * @param {HTMLElement} target   The capturing HTML element which defined a [data-action].
+   */
+  static async #deleteShape(event, target) {
+    const objectSize = this.document.system.combat.size;
+    const idx = target.closest("[data-shape-index]").dataset.shapeIndex;
+    await this.document.update({ "system.combat.size.shapes": objectSize._source.shapes.toSpliced(idx, 1) });
+  }
+
+  /* -------------------------------------------------- */
+
+  /**
+   * Delete all shapes.
+   * @this ObjectSizesInput
+   * @param {PointerEvent} event   The originating click event.
+   * @param {HTMLElement} target   The capturing HTML element which defined a [data-action].
+   */
+  static async #shapeRemoveAll(event, target) {
+    await this.document.update({ "system.combat.size.shapes": [] });
   }
 }
