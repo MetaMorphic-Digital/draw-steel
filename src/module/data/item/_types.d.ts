a44b7f1d2c82de2c2463e1e715bee18d8b73f3d2
Lusunati <Lusunati@gmail.com>
2026-09-19 22:54:38 +0100

    Initial Commit
    Adds a data path that is used to pass CriticalThresholdModifier.
    
    Modifier is subtracted from 19. It might be that we switch to the data path being 19, and then modified via +1/-1 for a more literal interpretation.

--- src/module/data/item/_types.d.ts
+++ src/module/data/item/_types.d.ts
@@ -105,6 +105,7 @@ declare module "./ability.mjs" {
       roll: {
         characteristics: Set<string>;
         reactive: boolean;
+        criticalThresholdModifier: number;
         formula: string;
         /** Non-persisted. */
         banes: number;
