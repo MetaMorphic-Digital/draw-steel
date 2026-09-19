c5a89f9576b4ba008c379fe10050ebe1e2b6d5af
Lusunati <Lusunati@gmail.com>
2026-09-20 00:55:28 +0100

    Update ability.mjs

--- src/module/data/item/ability.mjs
+++ src/module/data/item/ability.mjs
@@ -540,7 +540,6 @@ export default class AbilityModel extends BaseItemModel {
    */
   async use(config = {}, dialogOptions = {}, messageOptions = {}) {
     if (!this.actor) throw new Error("Abilities can only be used while embedded");
-
     const coreResource = this.actor.system.coreResource ?? {};
 
     const dialogConfig = foundry.utils.mergeObject({
